80a44f4bec852ee61cf8eb8c0908721253263ea0
furkannkaymaz <74837526+furkannkaymaz@users.noreply.github.com>
2022-08-01 11:39:05 +0300

    Create ToastExtension.kt

---
+++ app/src/main/java/com/furkan/beinConnectMovies/utils/extensions/ToastExtension.kt
@@ -0,0 +1,7 @@
+package com.furkan.beinConnectMovies.utils.extensions
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(message: String) =
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
